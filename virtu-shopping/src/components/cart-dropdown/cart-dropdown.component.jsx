d63b4a3935b3927c821427e3e033a88439f3a43f
shaik shahinsha <shahinsha.skh@gmail.com>
2024-03-22 18:37:25 +0530

    added shopping cart related data

--- virtu-shopping/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ virtu-shopping/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,17 @@
 import Button from '../button/button.component';
 import './cart-dropdown.styles.scss';
+import CartItem from '../cart-item/cart-item.component';
+import { useContext } from 'react';
+import { CartToggleContext } from '../../contexts/carttoggle.context';
+
 const CartDropDown = () => {
+    const {cartItems} = useContext(CartToggleContext);
     return(
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-
+                {cartItems.map((cartItem)=>{
+                   return (<CartItem key={cartItem.id} cartItem={cartItem}/>);
+                })}
             </div>
              <Button>GO TO CHEKOUT</Button>
         </div>
--- virtu-shopping/src/contexts/carttoggle.context.jsx
+++ virtu-shopping/src/contexts/carttoggle.context.jsx
@@ -1,13 +1,30 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems,productToAdd)=>{
+    const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id);
+
+    if(existingCartItem){
+      return cartItems.map((cartItem) => 
+           cartItem.id === productToAdd.id ? {...cartItem, quantity: cartItem.quantity + 1} : cartItem
+        );
+    }
+    //return new array if empty
+    return ([...cartItems, {...productToAdd, quantity:1}]);
+}
 export const CartToggleContext = createContext({
     isCartOpen : false,
-    setIsCartOpen : () => null
+    setIsCartOpen : () => null,
+    cartItems : [],
+    addItemToCart : () =>{}
 });
 
 export const CartToggleProvider = ({children}) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
-    const value = {isCartOpen, setIsCartOpen};
+    const [cartItems, setCartItems] = useState([]);
+    const addItemToCart = (productToAdd) =>{
+        setCartItems(addCartItem(cartItems,productToAdd));
+    }
+    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems};
     return (
         <CartToggleContext.Provider value={value}>{children}</CartToggleContext.Provider>
     )
